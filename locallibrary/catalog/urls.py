ed26dad442c7cc67dbf6c293066ff5a64603e54d
lulu <mluxunqi@gmail.com>
2025-04-07 01:53:39 +0200

    Advanced configuration

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -9,25 +9,3 @@ urlpatterns = [
     path('', RedirectView.as_view(url='catalog/')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-
-# admin.site.register(Author)
-# Define the admin class
-class AuthorAdmin(admin.ModelAdmin):
-    pass
-
-# Register the admin class with the associated model
-admin.site.register(Author, AuthorAdmin)
-
-# admin.site.register(Book)
-# admin.site.register(BookInstance)
-
-# Register the Admin classes for Book using the decorator
-@admin.register(Book)
-class BookAdmin(admin.ModelAdmin):
-    pass
-
-# Register the Admin classes for BookInstance using the decorator
-@admin.register(BookInstance)
-class BookInstanceAdmin(admin.ModelAdmin):
-    list_display = ('book', 'status', 'due_back')
-    list_filter = ('status', 'due_back')
