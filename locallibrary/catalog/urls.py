a2dbbad5f5b49cfc5dd02b94e35475842cc03234
lulu <mluxunqi@gmail.com>
2025-04-07 11:41:53 +0200

    registering modelAdmin class changing view of each model (class) adding filters controlling which fields are displayed and displaying inline classes

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -1,11 +1,11 @@
-from django.urls import include
-from django.views.generic import RedirectView
-from django.conf import settings
-from django.conf.urls.static import static
+
+from django.contrib import admin
+
+from django.urls import path, include
+from . import views
+
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('catalog/', include('catalog.urls')),
-    path('', RedirectView.as_view(url='catalog/')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
+    path('', views.index, name='index'),
+]
